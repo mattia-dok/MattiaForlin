d11059b716546181d1a005b0a2efaf53b4afd5c1
mattia-dok <mtt.forlin9@gmail.com>
2022-01-26 12:08:59 +0100

    completed all pages

--- static/js/get-repos-data.js
+++ static/js/get-repos-data.js
@@ -8,11 +8,18 @@ const getResult = async () => {
   const a = await result;
   console.log(a);
   console.log("Array dimension: " + a.length);
+  document.getElementById("link-1").href = a[1].html_url;
   document.getElementById("project-1").innerHTML = a[1].name;
   document.getElementById("language-1").innerHTML = a[1].language;
   document.getElementById("stars-1").innerHTML = a[1].stargazers_count;
   document.getElementById("watchers-1").innerHTML = a[1].watchers_count;
   document.getElementById("description-1").innerHTML = a[1].description;
+  document.getElementById("link-2").href = a[2].html_url;
+  document.getElementById("project-2").innerHTML = a[2].name;
+  document.getElementById("language-2").innerHTML = a[2].language;
+  document.getElementById("stars-2").innerHTML = a[2].stargazers_count;
+  document.getElementById("watchers-2").innerHTML = a[2].watchers_count;
+  document.getElementById("description-2").innerHTML = a[2].description;
 };
 
 getResult();
