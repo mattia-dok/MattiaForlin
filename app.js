7c667ebe28d920ec1cdb0428796f30ab85b79494
mattia-dok <mtt.forlin9@gmail.com>
2022-02-13 19:08:27 +0100

    added email system + more changes

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 
 const app = express();
 
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const config = require('./config');
 const axios = require('axios');
+const path = require('path');
+const multer = require('multer');
+const fs = require('fs');
+
+require("dotenv").config();
+
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -42,9 +50,7 @@ app.get('/aboutme', (req, res) => {
 });
 
 app.get('/contacts', (req, res) => {
-  res.render('pages/contacts/contacts', {
-    page: 'contacts'
-  });
+  res.sendFile(path.join(__dirname, '/views/pages/contacts/contacts.html'))
 });
 
 app.get('/members', (req, res) => {
@@ -56,9 +62,9 @@ app.get('/members', (req, res) => {
 app.get("/api/repos", (req, res) => {
   axios({
     method: "get",
-    url: `https://api.github.com/users/${config.githubUsername}/repos`,
+    url: `https://api.github.com/users/${process.env.GITHUB_USERNAME}/repos`,
     headers: {
-      Authorization: `Bearer ${config.githubToken}`,
+      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
       "Content-Type": "application/json",
       "Accept": "application/vnd.github.mercy-preview+json"
     }
@@ -66,6 +72,102 @@ app.get("/api/repos", (req, res) => {
     res.send(response.data);
   }).catch(err => {
     res.send(err);
+  });
+});
+
+const Storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./attachments");
+  },
+  filename: function (req, file, callback) {
+    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
+  },
+});
+
+const attachmentUpload = multer({
+  storage: Storage,
+}).single("attachment");
+
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.OAUTH_CLIENT_ID,
+    process.env.OAUTH_CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.OAUTH_REFRESH_TOKEN,
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject("Failed to create access token : error message(" + err);
+      }
+      resolve(token);
+    });
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL, accessToken,
+      clientId: process.env.OAUTH_CLIENT_ID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    },
+  });
+
+  return transporter;
+};
+
+app.post("/send_email", (req, res) => {
+  attachmentUpload(req, res, async function (error) {
+    if (error) {
+      return res.send("Error uploading file");
+    } else {
+      const name = req.body.name;
+      const sender = req.body.email-address;
+      const mailSubject = req.body.subject;
+      const mailBody = req.body.message;
+      const attachmentPath = req.file?.path;
+
+      let mailOptions = {
+        from: sender,
+        to: process.env.EMAIL,
+        subject: mailSubject,
+        text: name+" "+mailBody,
+        attachments: [
+          {
+            path: attachmentPath,
+          },
+        ],
+      };
+
+      try {
+        let emailTransporter = await createTransporter();
+
+        emailTransporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent: " + info.response);
+
+            fs.unlink(attachmentPath, function (err) {
+              if (err) {
+                return res.end(err);
+              } else {
+                console.log(attachmentPath + " has been deleted");
+                return res.sendFile(path.join(__dirname, '/views/pages/contacts/success.html'));
+              }
+            });
+          }
+        });
+      } catch (error) {
+        return console.log(error);
+      }
+    }
   });
 });
 
